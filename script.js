f22752e82f03f6bede2bac07a55191b3a5d0031f
jsbelle29 <yuihirasawa2973@gmail.com>
2024-07-13 03:11:31 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('.socials-btn').addEventListener('click', function() {
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+});
+
